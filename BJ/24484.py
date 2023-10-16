94ca970390f3da1be3cf128bc67c147d5c8f610b
engh0205@naver.com <engh0205@naver.com>
2023-10-16 21:56:13 +0900

    Algorithm

---
+++ BJ/24484.py
@@ -0,0 +1,42 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+input = sys.stdin.readline
+
+def DFS(graph,v,visited,depth):
+    global cnt
+
+    visited[v] = cnt
+    deep[v] = depth
+
+    for i in graph[v]:
+        if not visited[i]:
+            cnt+=1
+            DFS(graph,i,visited,depth+1)
+
+
+
+n,m,r = map(int,input().split())
+
+graph = [[] for _ in range(n+1)]
+visited = [0]*(n+1)
+deep = [-1] * (n+1)
+cnt = 1
+result = []
+
+
+for i in range(m):
+    u,v = map(int,input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+graph = [sorted(row, reverse=True) for row in graph]
+DFS(graph,r,visited,0)
+
+
+
+for i in range(1,n+1):
+    temp = visited[i] * deep[i]
+    result.append(temp)
+
+print(sum(result))
