b54cf7472ef6ed754e454e61afe64e2313469190
engh0205@naver.com <engh0205@naver.com>
2023-12-26 17:33:23 +0900

    Algorithm

---
+++ Java/prac/src/code/BubbleSort.java
@@ -0,0 +1,40 @@
+package code;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BubbleSort {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int list[] = new int[10];
+        for(int i = 0; i < 10; i++){
+            list[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int temp = 0;
+        int idx = 0;
+        for(int i = 1; i<10; i++){
+            for (int j=0; j<10-i; j++){
+
+                if(list[j] > list[j+1]){
+                    temp = list[j];
+                    list[j] = list[j+1];
+                    list[j+1] = temp;
+                }
+            }
+        }
+
+        for(int i=0; i<10; i++){
+            System.out.println(list[i]);
+        }
+
+
+
+
+
+    }
+}
