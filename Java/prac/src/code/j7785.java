267f50a0d94f59ba792193d0a2b337454dedcb5f
engh0205@naver.com <engh0205@naver.com>
2024-03-30 18:59:18 +0900

    Algorithm

---
+++ Java/prac/src/code/j7785.java
@@ -0,0 +1,40 @@
+package code;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class j7785 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        HashMap<String,String> log = new HashMap<>();
+
+        for(int i=0; i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String check = st.nextToken();
+            if(check.equals("enter")){
+                log.put(name,check);
+            }else{
+                log.remove(name);
+            }
+        }
+
+        Set<String> temp = log.keySet();
+
+        List<String> people = new ArrayList<>(temp);
+
+        people.sort(Collections.reverseOrder());
+
+        for (String person : people) {
+            bw.append(person + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
