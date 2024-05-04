fdd792fe20b0597991ab36b7d8209ca8249e56c5
engh0205@naver.com <engh0205@naver.com>
2024-05-04 10:50:19 +0900

    Algorithm

--- Java/prac/src/code/j10026.java
+++ Java/prac/src/code/j10026.java
@@ -10,41 +10,41 @@ public class j10026 {
     static boolean[][] visited_b;
     static boolean[][] visited_w;
     static int n;
-    static int[] dx = {0,1,0,-1};
-    static int[] dy = {1,0,-1,0};
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         basic = new char[n][n];
         weak = new char[n][n];
-        visited_b =new boolean[n][n];
-        visited_w =new boolean[n][n];
+        visited_b = new boolean[n][n];
+        visited_w = new boolean[n][n];
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             String temp = br.readLine();
             basic[i] = temp.toCharArray();
             weak[i] = temp.toCharArray();
         }
         int cnt_b = 0;
 
-        for(int i = 0;i<n;i++){
-            for(int j=0; j<n;j++){
-                if(weak[i][j] == 'G'){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (weak[i][j] == 'G') {
                     weak[i][j] = 'R';
                 }
-                if(!visited_b[i][j]){
-                    DFS_b(i,j);
+                if (!visited_b[i][j]) {
+                    DFS_b(i, j);
                     cnt_b++;
                 }
             }
         }
 
         int cnt_w = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(!visited_w[i][j]){
-                    DFS_w(i,j);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited_w[i][j]) {
+                    DFS_w(i, j);
                     cnt_w++;
                 }
             }
@@ -54,16 +54,16 @@ public class j10026 {
 
     }
 
-    private static void DFS_b(int i,int j){
-        if(visited_b[i][j]){
+    private static void DFS_b(int i, int j) {
+        if (visited_b[i][j]) {
             return;
         }
         visited_b[i][j] = true;
 
-        for(int k=0;k<4;k++){
+        for (int k = 0; k < 4; k++) {
             int x = i + dx[k];
             int y = j + dy[k];
-            if(x>=0 && y>=0 && x<n && y <n) {
+            if (x >= 0 && y >= 0 && x < n && y < n) {
                 if (!visited_b[x][y] && basic[i][j] == basic[x][y]) {
                     DFS_b(x, y);
                 }
@@ -71,16 +71,16 @@ public class j10026 {
         }
     }
 
-    private static void DFS_w(int i,int j){
-        if(visited_w[i][j]){
+    private static void DFS_w(int i, int j) {
+        if (visited_w[i][j]) {
             return;
         }
         visited_w[i][j] = true;
 
-        for(int k=0;k<4;k++){
+        for (int k = 0; k < 4; k++) {
             int x = i + dx[k];
             int y = j + dy[k];
-            if(x>=0 && y>=0 && x<n && y <n) {
+            if (x >= 0 && y >= 0 && x < n && y < n) {
                 if (!visited_w[x][y] && weak[i][j] == weak[x][y]) {
                     DFS_w(x, y);
                 }
--- Java/prac/src/code/j8958.java
+++ Java/prac/src/code/j8958.java
@@ -5,24 +5,25 @@ import java.util.Arrays;
 
 public class j8958 {
     static int T;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         T = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<T;i++){
+        for (int i = 0; i < T; i++) {
             String temp = br.readLine();
             char[] quiz = temp.toCharArray();
             int[] result = new int[quiz.length];
-            if(quiz[0] == 'O'){
+            if (quiz[0] == 'O') {
                 result[0] = 1;
-            }else{
+            } else {
                 result[0] = 0;
             }
-            for(int j=1;j<quiz.length;j++){
-                if(quiz[j] == 'O'){
-                    result[j] = result[j-1] + 1;
-                }else{
+            for (int j = 1; j < quiz.length; j++) {
+                if (quiz[j] == 'O') {
+                    result[j] = result[j - 1] + 1;
+                } else {
                     result[j] = 0;
                 }
             }
