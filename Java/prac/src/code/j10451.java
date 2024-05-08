df7d10f03886a2b7f83b5021c95ffd6dccd686c9
engh0205@naver.com <engh0205@naver.com>
2024-05-08 23:50:23 +0900

    Algorithm

--- Java/prac/src/code/j10451.java
+++ Java/prac/src/code/j10451.java
@@ -9,29 +9,30 @@ import java.util.StringTokenizer;
 public class j10451 {
     static boolean visited[];
     static ArrayList<Integer>[] num;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int t = Integer.parseInt(br.readLine());
 
-        for(int x=0;x<t;x++){
+        for (int x = 0; x < t; x++) {
             int n = Integer.parseInt(br.readLine());
             int cnt = 0;
-            num = new ArrayList[n+1];
-            visited = new boolean[n+1];
+            num = new ArrayList[n + 1];
+            visited = new boolean[n + 1];
             st = new StringTokenizer(br.readLine());
-            for(int i=1;i<=n;i++){
+            for (int i = 1; i <= n; i++) {
                 num[i] = new ArrayList<>();
             }
 
-            for(int i=1;i<=n;i++){
+            for (int i = 1; i <= n; i++) {
                 int temp = Integer.parseInt(st.nextToken());
                 num[i].add(temp);
                 num[temp].add(i);
             }
 
-            for(int i=1;i<=n;i++){
-                if(!visited[i]){
+            for (int i = 1; i <= n; i++) {
+                if (!visited[i]) {
                     search(i);
                     cnt++;
                 }
@@ -44,7 +45,7 @@ public class j10451 {
         visited[start] = true;
 
         for (Integer next : num[start]) {
-            if(!visited[next]){
+            if (!visited[next]) {
                 search(next);
             }
         }
