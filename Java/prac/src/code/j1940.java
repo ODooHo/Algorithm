c54d82e251144d43f0f111607dd53d5c3e424ae0
engh0205@naver.com <engh0205@naver.com>
2024-05-28 13:29:31 +0900

    Algorithm

--- Java/prac/src/code/j1940.java
+++ Java/prac/src/code/j1940.java
@@ -14,32 +14,29 @@ public class j1940 {
 
         int[] material = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             material[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(material);
 
         int start = 0;
-        int end = N-1;
+        int end = N - 1;
         int cnt = 0;
 
-        while(start<end){
+        while (start < end) {
             int temp = material[start] + material[end];
-            if (temp == M){
-                cnt+=1;
-                start+=1;
-                end -=1;
-            }
-            else if (temp > M){
-                end -=1;
-            }
-            else if (temp < M){
-                start+=1;
+            if (temp == M) {
+                cnt += 1;
+                start += 1;
+                end -= 1;
+            } else if (temp > M) {
+                end -= 1;
+            } else if (temp < M) {
+                start += 1;
             }
         }
 
         System.out.println(cnt);
-
 
 
     }
