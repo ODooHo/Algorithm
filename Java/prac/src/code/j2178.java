5a1cbc39208a733aaa6f0309a143f174da6fc191
engh0205@naver.com <engh0205@naver.com>
2024-05-31 23:31:07 +0900

    Algorithm

--- Java/prac/src/code/j2178.java
+++ Java/prac/src/code/j2178.java
@@ -9,9 +9,9 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class j2178 {
-    static int[] dx = {0,1,0,-1};
-    static int[] dy = {1,0,-1,0};
-    static boolean [][] visited;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static boolean[][] visited;
     static int n;
     static int m;
     static int[][] A;
@@ -25,29 +25,29 @@ public class j2178 {
         A = new int[n][m];
         visited = new boolean[n][m];
 
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             String line = st.nextToken();
-            for(int j=0; j<m; j++){
-                A[i][j] = Integer.parseInt(line.substring(j,j+1));
+            for (int j = 0; j < m; j++) {
+                A[i][j] = Integer.parseInt(line.substring(j, j + 1));
             }
         }
-        BFS(0,0);
+        BFS(0, 0);
 
-        System.out.println(A[n-1][m-1]);
+        System.out.println(A[n - 1][m - 1]);
     }
 
-    private static void BFS(int i, int j){
+    private static void BFS(int i, int j) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {i,j});
+        queue.offer(new int[]{i, j});
         visited[i][j] = true;
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int now[] = queue.poll();
-            for(int k=0; k<4;k++){  //4방향 탐색
+            for (int k = 0; k < 4; k++) {  //4방향 탐색
                 int x = now[0] + dx[k];
                 int y = now[1] + dy[k];
-                if(x>=0 && y>=0 && x<n && y<m){
-                    if(A[x][y] != 0 && !visited[x][y]){
+                if (x >= 0 && y >= 0 && x < n && y < m) {
+                    if (A[x][y] != 0 && !visited[x][y]) {
                         visited[x][y] = true;
                         A[x][y] = A[now[0]][now[1]] + 1;
                         queue.offer(new int[]{x, y});
