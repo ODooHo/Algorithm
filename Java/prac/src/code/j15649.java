a98edd3d7b6992fbd54fd45b5254e1a511343c1d
engh0205@naver.com <engh0205@naver.com>
2024-06-09 23:33:56 +0900

    Algorithm

--- Java/prac/src/code/j15649.java
+++ Java/prac/src/code/j15649.java
@@ -9,6 +9,7 @@ public class j15649 {
     static int N;
     static int M;
     static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -28,8 +29,8 @@ public class j15649 {
 
     }
 
-    static void dfs(int depth){
-        if(depth == M){
+    static void dfs(int depth) {
+        if (depth == M) {
             for (int i : arr) {
                 sb.append(i + " ");
             }
@@ -38,12 +39,12 @@ public class j15649 {
         }
 
 
-        for(int i=0; i<N; i++){
-            if(!visited[i]){
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
 
                 visited[i] = true;
-                arr[depth] = i+1;
-                dfs(depth+1);
+                arr[depth] = i + 1;
+                dfs(depth + 1);
 
                 visited[i] = false;
             }
