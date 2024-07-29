8691f0c1744207e23aa948c7d14f3b3f42e545bc
engh0205@naver.com <engh0205@naver.com>
2024-07-29 20:59:27 +0900

    Algorithm

--- Java/prac/src/code/test.java
+++ Java/prac/src/code/test.java
@@ -1,78 +1,56 @@
 package code;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class test {
-    static int[][] data = new int[4][8];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for(int i=0; i<4; i++) {
-            String line = br.readLine();
-            for(int j=0; j<8; j++) {
-                data[i][j] = line.charAt(j)-'0';
-            }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] A = new int[N];
+        int[] S = new int[N];
+
+        for(int i=0; i<N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
-        StringTokenizer st;
-        int k = Integer.parseInt(br.readLine());
-        for(int i=0; i<k; i++) {
+        S[0] = A[0];
+
+        for(int i=1; i<N; i++){
+            S[i] = S[i-1] + A[i];
+        }
+
+        for(int i=0;i<M;i++){
             st = new StringTokenizer(br.readLine());
-            int idx = Integer.parseInt(st.nextToken())-1;
-            int dir = Integer.parseInt(st.nextToken());
-            gearOperation(idx,dir);
-        }
+            int f = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
 
-        for (int[] datum : data) {
-            for (int i : datum) {
-                System.out.print(i);
+            if(f == s){
+                bw.append(A[f-1] + "\n");
             }
-            System.out.println();
-        }
-        int total =0;
-        for(int i=0; i<4; i++) {
-            total += Math.pow(2, i)*data[i][0];
-        }
-        System.out.println(total);
-    }
-
-    static void gearOperation(int idx, int dir) {
-        // 왼쪽 (0 ~ idx-1)
-        leftSide(idx-1, -dir);
-        // 오른쪽 (idx+1 ~ 3)
-        rightSide(idx+1, -dir);
-        rotation(idx,dir);
-    }
-
-    static void leftSide(int idx, int dir) {
-        if(idx < 0) return;
-        if(data[idx][2]==data[idx+1][6]) return;
-        leftSide(idx-1, -dir);
-        rotation(idx,dir);
-    }
-
-    static void rightSide(int idx, int dir) {
-        if(idx > 3) return;
-        if(data[idx][6] == data[idx-1][2]) return;
-        rightSide(idx+1, -dir);
-        rotation(idx,dir);
-    }
-
-    static void rotation(int idx, int dir) {
-        if(dir==1) {
-            int tmp = data[idx][7];
-            for(int i=7; i>0; i--) {
-                data[idx][i] = data[idx][i-1];
+            else if (f == 1){
+                bw.append(S[s-1] + "\n");
             }
-            data[idx][0] = tmp;
-        }else {
-            int tmp = data[idx][0];
-            for(int i=0; i<7; i++) {
-                data[idx][i] = data[idx][i+1];
+            else{
+                int temp =  S[s-1] - S[f-2];
+
+                bw.append(temp + "\n");
             }
-            data[idx][7] = tmp;
         }
+
+        bw.flush();
+        bw.close();
+
+
     }
+
 }
