2126e99e6f56117eee43d9a2bdf5e3b5c3889318
engh0205@naver.com <engh0205@naver.com>
2024-07-30 20:40:09 +0900

    Algorithm

--- Java/prac/src/code/j2607.java
+++ Java/prac/src/code/j2607.java
@@ -13,38 +13,38 @@ public class j2607 {
         int N = Integer.parseInt(br.readLine());
 
         String first = br.readLine();
-        char[] temp = first.toCharArray();
-        Set<Character> standard = new HashSet<>();
-        for (char c : temp) {
-            standard.add(c);
-        }
-        int cnt = 0;
-        for(int i=0; i<N-1; i++){
-            Set<Character> check = new HashSet<>();
-            String word = br.readLine();
-            char[] words = word.toCharArray();
-            for (char c : words) {
-                check.add(c);
+        int result = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            String temp = br.readLine();
+            int cnt = 0;
+            int[] word = new int[26];
+            for (int j = 0; j < first.length(); j++) {
+                word[first.charAt(j) - 'A']++;
             }
-            if(standard.equals(check)){
-                if(first.length()+1>=word.length()){
+
+            for (int j = 0; j < temp.length(); j++) {
+                if (word[temp.charAt(j) - 'A'] > 0) {
                     cnt++;
+                    word[temp.charAt(j) - 'A']--;
                 }
             }
-            else if(first.length() == word.length()){
-                int stack = 0;
-                for (Character s : standard) {
-                    for (Character c : check) {
-                        if(s==c){
-                            stack++;
-                        }
-                    }
-                }
-                if(stack==first.length()-1){
-                    cnt++;
-                }
+
+            // 기준 문자열 길이와 비교 문자열 길이 차이
+            // 같은 경우 or 한 글자만 바뀐경우
+            if (first.length() == temp.length() && (first.length() == cnt || first.length() - 1 == cnt)) {
+                result++;
             }
+            // 비교 문자열이 짧은 경우 (제거)
+            else if (first.length() == temp.length() - 1 && temp.length() - 1 == cnt) {
+                result++;
+            }
+            // 비교 문자열이 긴 경우 (추가)
+            else if (first.length() == temp.length() + 1 && temp.length() == cnt) {
+                result++;
+            }
+
         }
-        System.out.println(cnt);
+        System.out.println(result);
     }
 }
