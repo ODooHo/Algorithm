5946daf22984717668ac793882009976e2692407
engh0205@naver.com <engh0205@naver.com>
2024-08-25 00:57:30 +0900

    Kotlin_tutorial

--- Java/prac/src/code/kotlin/prac.kt
+++ Java/prac/src/code/kotlin/prac.kt
@@ -1,4 +1,28 @@
 package code.kotlin
 
-class prac {
+import java.io.BufferedReader
+
+
+fun main() {
+    var temp = Integer.valueOf(readlnOrNull())
+    var a = ""
+
+    if (temp == null) {
+        print("Wrong INPUT!!")
+    } else {
+
+        if (temp >= 70) {
+            a = "A"
+        } else if (temp >= 50 && temp < 70) {
+            a = "B"
+        } else if (temp >= 30 && temp < 50) {
+            a = "C"
+        } else {
+            a = "D"
+        }
+
+        print(a)
+    }
+
+
 }
