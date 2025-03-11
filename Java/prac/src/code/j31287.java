aee5a5416fed2d3e1743f2afb895a07cb55acff3
ODooHo <engh0205@naver.com>
2025-03-11 13:41:29 +0900

    j31287

--- Java/prac/src/code/j31287.java
+++ Java/prac/src/code/j31287.java
@@ -3,6 +3,10 @@ package code;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -10,41 +14,39 @@ public class j31287 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        String move = br.readLine();
-        for (int i = 0; i < k; i++) {
-            move += move;
-        }
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
+        char[] temp = br.readLine().toCharArray();
         int x = 0;
         int y = 0;
+
         boolean flag = false;
-        for (int i = 0; i < n; i++) {
-            switch (move.charAt(i)) {
-                case 'U':
-                    y++;
-                    break;
-                case 'D':
-                    y--;
-                    break;
-                case 'R':
-                    x++;
-                    break;
-                case 'L':
-                    x--;
-                    break;
-            }
-            if (x == 0 && y == 0) {
-                flag = true;
-                System.out.println("YES");
-                break;
+        String result = "NO";
+
+        for (int i = 0; i < K; i++) {
+            for (char current : temp) {
+                switch (current){
+                    case 'U':
+                        x++; // x 증가
+                        break;
+                    case 'R':
+                        y++; // y 증가
+                        break;
+                    case 'L':
+                        y--; // y 감소
+                        break;
+                    case 'D':
+                        x--; // x 감소
+                        break;
+                }
+                if(x == 0 && y == 0){
+                    result = "YES";
+                    System.out.print(result);
+                    return;
+                }
             }
         }
-        if (!flag) {
-            System.out.println("NO");
-        }
-
-
+        System.out.print(result);
     }
 }
